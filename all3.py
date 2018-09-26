ac98fef432a88f04f103d2ad18df2dc623ffd268
9535453353 <42990248+9535453353@users.noreply.github.com>
2018-09-26 10:32:34 +0530

    Add files via upload

---
+++ all3.py
@@ -0,0 +1,30 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+o=input("what do u want? +,-,*,/:")
+
+def add():
+	return i+j
+def sub(a,b):
+	return a-b
+def multi(a,b):
+	return a*b
+def divide(a,b):
+	return a/b
+
+
+if(o=="-"):
+	print(sub())
+elif(o=="+"):
+    print(add())
+elif(o=="*"):
+     print(multi())
+elif(o=="/"):
+     print(divide())       	
+		
+
+
+
+
+
+	
+	
